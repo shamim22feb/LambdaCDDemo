a2c1143ee8236e5b02e1ce56b9ece9f84d56ac5a
shamim22feb <shamim22feb@gmail.com>
2020-07-02 21:01:18 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 
-ec2 = boto3.client('ec2')
+#ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response ={"h1":"hello, it is working. i am right"}
     return {"statusCode": 200, "body": json.dumps(response)}
